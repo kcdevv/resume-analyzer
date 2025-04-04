7eac60389d9ef87a1ee1aa78aeff326b454e8eb7
krisnachaitanya <stealgamerofficial@gmail.com>
2025-04-04 14:31:24 +0000

    added sample

--- be/src/index.ts
+++ be/src/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, "files/");
   },
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + "-" + file.originalname;
